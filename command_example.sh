e122eed38f9ef909dfdf7eb4806a9da02ba1f8ae
terner-running-terminal-commands <runningterminalcommands@gmail.com>
2026-03-08 14:56:31 +0900

    first commit

---
+++ command_example.sh
@@ -0,0 +1,18 @@
+# ./command_example.sh
+
+
+curl --head https://www.nihon-u.ac.jp/ > nihon.txt
+
+curl --head https://www.toyo.ac.jp/ > toyo.txt
+
+curl --head https://www.komazawa-u.ac.jp/ > komazawa.txt
+
+curl --head https://www.senshu-u.ac.jp/ > senshu.txt
+
+curl --head https://ss523971.stars.ne.jp/todo/
+
+curl https://ss523971.stars.ne.jp/todo/ > todo.txt
+
+curl https://ss523971.stars.ne.jp/todo/hello/
+
+
